a591ad601087057bab64c9a326b2a899fb862cff
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2017-01-16 12:59:59 +0100

    bot response directly

--- bot.js
+++ bot.js
@@ -2,31 +2,53 @@
 console.log("Up 'n' running… 😎 \n");
 
 // Import. library.
-var Twit = require('twit');
+const Twit = require('twit');
 
 // Importing the access configuration.
-var Config = require('./config');
+const Config = require('./config');
 
-// LOgging in a variable.
-var T = new Twit(Config);
+// Logging in a variable.
+const T = new Twit(Config);
 
-tweetIt();
-
-function tweetIt(){
+// Handle the resopnse. 
+function tweetIt(r){
 	var tweet = {
-	status: " " ,
+	status: r ,
 	};
-	console.log(voc.lemmi[ord]);
-	ord += 1;
 
 	T.post('statuses/update', tweet, tweeted);
 
 	function tweeted(err, data, response){
 		if(err) {
-			console.log("Something went wrong… 😰 \n");
+			console.log("Something went wrong… 😰 \n" + err );
 		} else {
 			console.log("Everything went fine… 🙌 \n");
 		}
 	}
-
 }
+
+
+// Filter the twitter public stream by mention.
+const stream = T.stream('statuses/filter', { track: '@BotSibilla' })
+
+// Stream on!!!
+stream.on('tweet', function (tweet) {
+	// Logging the user
+	const usr = "@" + tweet.user.screen_name;
+	console.log("User: " + usr);
+	// Logging the text.
+	const string = tweet.text;
+	console.log("Text: " + string);
+
+	// Filtering the tweet.
+	// Fetching the first word after @BotSibilla.
+	const word = /^(?=.*\bBotSibilla\b)(?:\S+ ){1}(\S+)/ig.exec(string);
+	console.log("Word: " + word[1]);
+
+	// Tweets back the same word
+	const response =  usr + " " + word[1];
+
+	tweetIt(response);
+	console.log("Response: " + response);
+
+});
